81375e93c15076f9b0117d36d3bf90ef75c35aaa
Christian Hettlage <hettlage@saao.ac.za>
2022-06-21 22:10:53 +0200

    Docstring added

--- src/pytest_pymysql_snapshot_mock/util.py
+++ src/pytest_pymysql_snapshot_mock/util.py
@@ -50,6 +50,41 @@ class DatabaseMockFixture:
         return self._mode
 
     def user_value(self, value: Any) -> Any:
+        """
+        Mock a user-supplied value.
+
+        Some database tests may generate random values, store these in the database and
+        use these random values in assertions. This poses a problem for mocking as the
+        value will be different for each test run, so that the current and the
+        previously stored version differs. Hence such tests will fail when run with the
+        stored data.
+
+        The solution is to wrap random values with this method. For example:
+
+        .. code:: python
+
+           import uuid
+
+           random_value = database_mock.user_value(str(uuid.v4())
+
+        When database data is stored, ``value`` is stored along with the data, and it is
+        returned as the return value. When the database is mocked, the previously stored
+        stored value is returned. Otherwise the method just returns ``value``.
+
+        It must be possible to pickle the passed value
+
+        Parameters
+        ----------
+        value: any
+            Value. The value is ignored when the database is mocked.
+
+        Returns
+        -------
+        any
+            Either the previously stored value (when the database is being mocked) or
+            the passed value (otherwise).
+
+        """
         if self._mode == Mode.STORE_DATA:
             self._data[f"user--stored-value"].append(value)
             return value
